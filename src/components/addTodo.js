df279c0a55ff3547fdc200f41b4d5567fae8b77d
kevinsims1 <kevinsims1@outlook.com>
2020-04-10 23:31:05 -0700

    replaced div

--- src/components/addTodo.js
+++ src/components/addTodo.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { TextField } from '@material-ui/core'
+import { TextField, Box } from '@material-ui/core'
 import {CustomButton} from '../styles/Customs.js'
 
 //css
@@ -11,13 +11,13 @@ const AddTodo = (props) => {
 
 
     return (
-        <div className="addtodo">
+        <Box className="addtodo">
           <CustomButton variant="contained"  className="button"  size="large" onClick={()=>props.onClick()} >
             Add Todo
           </CustomButton>
           
           <TextField id="standard-basic"  value={props.newTodo} color="primary" label="New Todo" onChange={(e) => props.handleChange(e)}/>
-        </div>
+        </Box>
     )
 };
 
